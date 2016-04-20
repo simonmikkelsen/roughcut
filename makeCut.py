4e072af3b7044c416efabb7bbe141f31d45362ea
Simon Mikkelsen <simonmikkelsen@users.noreply.github.com>
2016-04-20 22:06:03 +0200

    Initial basic working version.

---
+++ makeCut.py
@@ -0,0 +1,49 @@
+#! /usr/bin/python
+
+import sys
+import os.path
+import json
+
+class InfoReader:
+  def __init__(self, filename):
+    self.filename = filename
+    fp = open(self.filename, "r")
+    self.info = json.load(fp)
+    fp.close()
+
+  def getInfo(self):
+    return self.info
+
+class InfoMerger:
+  def __init__(self, info):
+    self.info = info
+
+  def mergeInfo(self):
+    merged = []
+    # TODO: Implementer stop i skriveren.
+    prevFrameNo = -1
+    for entry in self.info:
+      frameno = entry['frameno']
+      if frameno < prevFrameNo:
+        i = 0
+        for m in merged:
+          if m['frameno'] > frameno:
+            break
+          else:
+            i = i+1
+        merged = merged[:i]
+      prevFrameNo = frameno
+      merged.append(entry)
+    return merged
+
+
+if __name__ == "__main__":
+  filename = sys.argv[1]
+  if not os.path.isfile(filename):
+    print "The given filename '%s' does not exist." % filename
+  infoReader = InfoReader(filename)
+  info = infoReader.getInfo()
+  merger = InfoMerger(info)
+  print merger.mergeInfo()
+
+
