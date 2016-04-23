b3c5323540fe6626894916f87615b8c643904f5b
Simon Mikkelsen <simon14@zip.dk>
2016-04-23 20:00:59 +0200

    Z

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+Dependencies
+------------
+pyexiftool - https://github.com/smarnach/pyexiftool
+vlc - http://www.videolan.org/
+mlt - https://www.mltframework.org/
--- indexmedia.py
+++ indexmedia.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python
 
 import sys
+import os
 import os.path
 import json
+import exiftool
+
 
 class MediaIndexer:
   def __init__(self, paths):
     self.paths = paths 
+    self.extensions = ('.mp4', '.mts', '.mov', '.avi', '.mpg', '.wav', '.mp3', '.gpx')
+    self.exiftool = exiftool.ExifTool()
+    self.exiftool.start()
   def doIndex(self):
     for path in paths:
-      print path
+      for dirName, subdirList, fileList in os.walk(path):
+        if dirName.find("\n") == -1:
+          for fname in fileList:
+            if fname.lower().endswith(self.extensions):
+              self.registerMedia(os.path.join(dirName, fname))
+
+  def registerMedia(self, mediaPath):
+    print self.exiftool.get_metadata(mediaPath)
+    # QuickTime:CreateDate
+    # QuickTime:TrackDuration
+    # QuickTime:VideoFrameRate
+    # H264:DateTimeOriginal
+    # H264:CaptureFrameRate
+    # H264:VideoFrameRate
+    # M2TS:Duration
 
 
 if __name__ == "__main__":
   paths = sys.argv[1:]
   indexer = MediaIndexer(paths)
+  indexer.doIndex()
