c5a59d2c2fb8f2f8dbb382d71a126dc4b3a5da40
Simon Mikkelsen <simon14@zip.dk>
2016-04-24 14:55:15 +0200

    Added support for rading exif.

--- indexmedia.py
+++ indexmedia.py
@@ -6,13 +6,38 @@ import os.path
 import json
 import exiftool
 
+class ExifReader:
+  def __init__(self):
+    self.exiftool = exiftool.ExifTool()
+    self.exiftool.start()
+
+  def read(self, mediaPath):
+    self.metadata = self.exiftool.get_metadata(mediaPath)
+
+  def get(self, key):
+    if key in self.metadata:
+      return self.metadata[key]
+    else:
+      return None
+  def getOneOf(self, keys):
+    for keyToFind in keys:
+      for key in self.metadata.keys():
+	if key.endswith(keyToFind):
+          return self.metadata[key]
+    return None
+
+  def getCreateDate(self):
+    return self.getOneOf(['CreateDate', 'DateTimeOriginal'])
+  def getFrameRate(self):
+    return self.getOneOf(['FrameRate'])
+  def getDuration(self):
+    return self.getOneOf(['Duration'])
 
 class MediaIndexer:
   def __init__(self, paths):
     self.paths = paths 
     self.extensions = ('.mp4', '.mts', '.mov', '.avi', '.mpg', '.wav', '.mp3', '.gpx')
-    self.exiftool = exiftool.ExifTool()
-    self.exiftool.start()
+    self.exifReader = ExifReader()
   def doIndex(self):
     for path in paths:
       for dirName, subdirList, fileList in os.walk(path):
@@ -22,7 +47,14 @@ class MediaIndexer:
               self.registerMedia(os.path.join(dirName, fname))
 
   def registerMedia(self, mediaPath):
-    print self.exiftool.get_metadata(mediaPath)
+    self.exifReader.read(mediaPath)
+    print "Date, duration, framerate"
+    print self.exifReader.getCreateDate()
+    print self.exifReader.getDuration()
+    print self.exifReader.getFrameRate()
+    print mediaPath
+    print
+
     # QuickTime:CreateDate
     # QuickTime:TrackDuration
     # QuickTime:VideoFrameRate
