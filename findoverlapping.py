5bbdfbe1d221b06d59f49354be1804ada0f19a1a
Simon Mikkelsen <simonmikkelsen@users.noreply.github.com>
2016-05-06 15:20:15 +0200

    More info printed to evaluate how much they files overlap.

--- findoverlapping.py
+++ findoverlapping.py
@@ -15,10 +15,20 @@ class OverlapFinder:
         #dr2.start > dr1.start -- start after dr1 is started
         #dr2.start < dr1.end   -- start before dr1 is finished
         if inMedia[1] > outMedia[1] and inMedia[1] < outMedia[2]:
-          overlapDuration = min(inMedia[2], outMedia[2])  - inMedia[1]
+          #overlapDuration = min(inMedia[2], outMedia[2])  - inMedia[1]
+          (overlapDuration, overlapPercent) = self.getOverlap(inMedia, outMedia)
           if overlapDuration > 2:
-            print "Overlap: " + str(overlapDuration)+" sec: " + str(outMedia[3]) + " / "  + str(inMedia[3])
+              print "Overlap: " + str(overlapDuration)+" sec by " + str(int(overlapPercent * 100)) + "%: "  + str(outMedia[3]) + " / "  + str(inMedia[3])
+  def getOverlap(self, inMedia, outMedia):
+    overlapDuration = min(inMedia[2], outMedia[2])  - inMedia[1]
 
+    firstStart = min(inMedia[1], outMedia[1])
+    lastEnd = max(inMedia[2], outMedia[2])
+    span = lastEnd - firstStart
+
+    percent = 1 - ((span - overlapDuration) / span)
+
+    return (overlapDuration, percent)
 if __name__ == "__main__":
   finder = OverlapFinder()
   print finder.findOverlaps()
