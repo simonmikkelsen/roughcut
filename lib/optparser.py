130725aa956c72a7ea2bedec8301d5e0fbd68b06
Simon Mikkelsen <simon14@zip.dk>
2016-06-01 21:19:46 +0200

    Now works.

--- lib/optparser.py
+++ lib/optparser.py
@@ -1,12 +1,25 @@
 class OptParser:
+  """Parses the options in the given file and allowed for the content to be returned.
+The file is expected to contain key/value pairs. Empty lines, and lines where first non
+white space is a # are ignored.
+"""
   def __init__(self, filename):
     self.filename = filename
   
   def parse(self):
     myvars = {}
-    with open(filename) as myfile:
+    with open(self.filename) as myfile:
       for line in myfile:
-        if line.strip().startswith('#'):
-i         continue
+        if line.strip().startswith('#') or len(line.strip()) == 0:
+          continue
         name, var = line.partition("=")[::2]
-        myvars[name.strip()] = var
+        myvars[name.strip()] = var.strip()
+    self.content = myvars
+  def getContent(self):
+    return self.content
+
+if __name__ == '__main__':
+  import sys
+  parser = OptParser(sys.argv[1])
+  parser.parse()
+  print parser.getContent()
