1e22b4b52fb2bed2948a81bcffc96c6d3495d59b
Simon Mikkelsen <simonmikkelsen@users.noreply.github.com>
2016-07-03 15:32:44 +0200

    Removed unused code.

--- roughcut.py
+++ roughcut.py
@@ -163,7 +163,7 @@ class Player(QtGui.QMainWindow):
             self.isPaused = True
         else:
             if self.mediaplayer.play() == -1:
-                self.OpenFile()
+                self.OpenFiles()
                 return
             self.mediaplayer.play()
             self.playbutton.setText("&Pause")
@@ -223,39 +223,6 @@ class Player(QtGui.QMainWindow):
         clipInfoFile = ClipInfoJsonFile(filename)
         self.ratingAdapter.setReceiver(clipInfoFile)
        
-        # parse the metadata of the file
-        self.media.parse()
-        # set the title of the track as window title
-        self.setWindowTitle(self.media.get_meta(0))
-
-        # the media player has to be 'connected' to the QFrame
-        # (otherwise a video would be displayed in it's own window)
-        # this is platform specific!
-        # you have to give the id of the QFrame (or similar object) to
-        # vlc, different platforms have different functions for this
-        if sys.platform.startswith('linux'): # for Linux using the X Server
-            self.mediaplayer.set_xwindow(self.videoframe.winId())
-        elif sys.platform == "win32": # for Windows
-            self.mediaplayer.set_hwnd(self.videoframe.winId())
-        elif sys.platform == "darwin": # for MacOS
-            self.mediaplayer.set_nsobject(self.videoframe.winId())
-        self.PlayPause()
-
-    def OpenFile(self, filename=None):
-        """Open a media file in a MediaPlayer
-        """
-        if filename is None:
-            filename = QtGui.QFileDialog.getOpenFileName(self, "Open File", os.path.expanduser('~'))
-        if not filename:
-            return
-
-        # create the media
-        if sys.version < '3':
-            filename = unicode(filename)
-        self.media = self.instance.media_new(filename)
-        # put the media in the media player
-        self.mediaplayer.set_media(self.media)
-
         # parse the metadata of the file
         self.media.parse()
         # set the title of the track as window title
